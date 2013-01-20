66f59cca0184dff5b86f11d2aef7472fe522af47
Stas A. Kraev <stanislav.kraev@clearscale.net>
2013-01-20 06:35:14 +0000

    Init commit for with zabbix cookbook template
    refs# https://jira.cloudnoc.com/browse/CNCUST-13

---
+++ cookbooks/zabbix/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "CloudNOC Inc"
+maintainer_email "stanislav.kraev@cloudnoc.com"
+license          "All rights reserved"
+description      "Installs/Configures zabbix"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.0.1"
