2247b9f985c1554ab24d2172846cdda854ecc411
Andrey Khon <andrey.khon@clearscale.net>
2013-01-23 05:14:21 +0700

    Fixed permissions of msi files

--- cookbooks/zabbix_windows/recipes/windows_agent.rb
+++ cookbooks/zabbix_windows/recipes/windows_agent.rb
@@ -31,32 +31,27 @@ end
 cookbook_file "#{installdir}\\#{installer}" do
 	source "#{installer}"
 	action :create_if_missing
-	inherits true
 end
 
 cookbook_file "#{installdir}\\#{getter}" do
 	source "#{getter}"
 	action :create_if_missing
-	inherits true
 end
 
 cookbook_file "#{installdir}\\#{sender}" do
 	source "#{sender}"
 	action :create_if_missing
-	inherits true
 end
 
 template "#{installdir}\\agent_include" do
 	source "agent_include.erb"
 	action :create_if_missing
-	inherits true
 	notifies :restart, "service[Zabbix Agent]"
 end
 
 template "#{installdir}\\zabbix_agentd.conf" do
 	source "zabbix_agentd.conf.erb"
 	action :create_if_missing
-	inherits true
 	notifies :restart, "service[Zabbix Agent]"
 end
 
@@ -70,18 +65,13 @@ if services.count >= 1
 	#service exists
 else
 	#INSTALL AWAY!
-
-	# Back to Chef-land
 	execute "Install Zabbix" do
 		command "#{installdir}\\#{installer} --config #{installdir}\\zabbix_agentd.conf --install"
 		#not_if { File.exists?("#{node['perl']['install_dir']}\\perl\\bin\\perl.exe") }
 	end
 end
 
-# Define zabbix_agentd service
-#service "zabbix_agentd" do
 service "Zabbix Agent" do
   supports :status => true, :start => true, :stop => true, :restart => true
-  action [ :enable ]
-  action [ :start ]
+  action [ :enable , :start]
 end
