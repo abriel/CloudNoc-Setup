192347deb80ce9a4ebbc33d0b297454bae35ca7b
Andrey Khon <andrey.khon@clearscale.net>
2013-01-23 05:03:46 +0700

    Fix metadata

--- cookbooks/zabbix_windows/metadata.rb
+++ cookbooks/zabbix_windows/metadata.rb
@@ -5,4 +5,4 @@ description      "Installs/Configures zabbix"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
 
-recipe "zabbix::windows_agent", "Installs and configures Zabbix agent"
+recipe "zabbix_windows::windows_agent", "Installs and configures Zabbix agent"
