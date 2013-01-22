d77f55b20832535348a5cd1186aa7892ad98424b
Andrey Khon <andrey.khon@clearscale.net>
2013-01-23 04:29:27 +0700

    First commit

---
+++ cookbooks/zabbix_windows/metadata.rb
@@ -0,0 +1,8 @@
+maintainer       "CloudNOC Inc"
+maintainer_email "stanislav.kraev@cloudnoc.com"
+license          "All rights reserved"
+description      "Installs/Configures zabbix"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.0.1"
+
+recipe "zabbix::windows_agent", "Installs and configures Zabbix agent"
---
+++ cookbooks/zabbix_windows/recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: zabbix
+# Recipe:: default
+#
+# Copyright 2013, CloudNOC
+#
+# All rights reserved - Do Not Redistribute
+#
+
+# Windows
+include_recipe "zabbix_windows::windows_agent"
