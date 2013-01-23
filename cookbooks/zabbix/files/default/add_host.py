f1b4abecad926df75debdd86114242b00951efe0
Dmitry Pisarev <spirit@live.1nsk.ru>
2013-01-24 01:48:07 +0700

    zabbix cookbook: add_host.py has been modified to standalone script, not template. Can take arguments from cmd. Now we can build it to win exe via pyinstaller.

--- cookbooks/zabbix/files/default/add_host.py
+++ cookbooks/zabbix/files/default/add_host.py
@@ -3,22 +3,27 @@
 # get it from github.com/gescheit/scripts.git
 
 from zabbix_api import ZabbixAPI
+import optparse
 
-server="@@SERVERNAME@@"
-username="@@USERNAME@@"
-password="@@PASSWORD@@"
+opt_parser = optparse.OptionParser(option_list = [
+	optparse.make_option('-s', '--server-name', dest = 'server_name'),
+	optparse.make_option('-u', '--username', dest = 'username'),
+	optparse.make_option('-p', '--password', dest = 'password'),
+	optparse.make_option('-n', '--hostname', dest = 'hostname'),
+	optparse.make_option('-d', '--description', dest = 'description'),
+	optparse.make_option('-c', '--clientname', dest = 'client_name'),
+	optparse.make_option('-e', '--clientemail', dest = 'client_email'),
+	optparse.make_option('-x', '--proxyhost', dest = 'proxy_host'),
+	optparse.make_option('-i', '--inetaddr', dest = 'inet_addr'),
+	optparse.make_option('-t', '--template', dest = 'template'),
+	])
 
-zapi = ZabbixAPI(server=server, path="", proto="https", user=username, passwd=password, log_level=6)
-zapi.login(username, password)
+options, args = opt_parser.parse_args()
 
-host_name="@@HOSTNAME@@"
+zapi = ZabbixAPI(server=options.server_name, path="", proto="https", user=options.username, passwd=options.password, log_level=6)
+zapi.login(options.username, options.password)
 
-description='@@DESCRIPTION@@' 
-clientname='@@CLIENTNAME@@'
-clientcontact='@@CLIETNEMAIL@@'
-proxy_host_name='@@PROXYHOSTNAME@@'
+proxyid=zapi.host.get({"filter":{'host': options.proxy_host}, 'output': 'extend', 'proxy_hosts': '1'})[0]["hostid"]
+groupid=zapi.hostgroup.get({"filter":{'name': options.client_name}, 'output': 'extend'})[0]["groupid"]
 
-proxyid=zapi.host.get({"filter":{'host': proxy_host_name}, 'output': 'extend', 'proxy_hosts': '1'})[0]["hostid"]
-groupid=zapi.hostgroup.get({"filter":{'name': clientname}, 'output': 'extend'})[0]["groupid"]
-
-zapi.host.create({ "host":host_name, "interfaces":{"type":"1", "main":"1", "useip":"1","ip":"@@AGENTIP@@", "dns":"", "port":"10050"}, "groups":{"groupid":groupid},"inventory":{"contact":clientcontact}, "templates":{ "templateid":"@@TMPL_ID@@"}, "proxy_hostid":proxyid  })
+zapi.host.create({ "host":options.hostname, "interfaces":{"type":"1", "main":"1", "useip":"1","ip":options.inet_addr, "dns":"", "port":"10050"}, "groups":{"groupid":groupid},"inventory":{"contact":options.client_email}, "templates":{ "templateid":options.template}, "proxy_hostid":proxyid  })
