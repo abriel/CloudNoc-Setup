66a79c24897aebcfa877f46a85c2eadf882e9ba8
Andrey Khon <andrey.khon@clearscale.net>
2013-01-23 16:28:05 +0700

    Added debug log strings

--- cookbooks/zabbix_windows/recipes/windows_agent.rb
+++ cookbooks/zabbix_windows/recipes/windows_agent.rb
@@ -14,6 +14,13 @@ installer	= "zabbix_agentd#{node['zabbix_windows']['agent']['windows_bitness']}.
 getter		= "zabbix_get#{node['zabbix_windows']['agent']['windows_bitness']}.exe"
 sender		= "zabbix_sender#{node['zabbix_windows']['agent']['windows_bitness']}.exe"
 
+log "Debug"
+log "installer: #{installer}"
+log "getter: #{getter}"
+log "sender: #{sender}"
+log "log dir: #{node['zabbix_windows']['agent']['include_dir']}"
+log "installdir: #{installdir}"
+log "include dir: #{node['zabbix_windows']['agent']['include_dir']}"
 
 #directory node['zabbix_windows']['agent']['include_dir'] do
 #	action :create
@@ -26,7 +33,6 @@ directory node['zabbix_windows']['log_dir'] do
 	recursive true
 	inherits true
 end
-
 
 cookbook_file "#{installdir}\\#{installer}" do
 	source "#{installer}"
@@ -43,35 +49,35 @@ cookbook_file "#{installdir}\\#{sender}" do
 	action :create_if_missing
 end
 
-template "#{installdir}\\agent_include" do
-	source "agent_include.erb"
-	action :create_if_missing
-	notifies :restart, "service[Zabbix Agent]"
-end
+#template "#{installdir}\\agent_include" do
+#	source "agent_include.erb"
+#	action :create_if_missing
+#	notifies :restart, "service[Zabbix Agent]"
+#end
 
-template "#{installdir}\\zabbix_agentd.conf" do
-	source "zabbix_agentd.conf.erb"
-	action :create_if_missing
-	notifies :restart, "service[Zabbix Agent]"
-end
+#template "#{installdir}\\zabbix_agentd.conf" do
+#	source "zabbix_agentd.conf.erb"
+#	action :create_if_missing
+#	notifies :restart, "service[Zabbix Agent]"
+#end
 
 
 # Check Windows Service
-require 'win32ole'
-zabbixservice = 'Zabbix Agent'
-wmi = WIN32OLE.connect("winmgmts://")
-services = wmi.ExecQuery("Select * from Win32_Service where Name = '#{zabbixservice}'")
-if services.count >= 1
-	#service exists
-else
-	#INSTALL AWAY!
-	execute "Install Zabbix" do
-		command "#{installdir}\\#{installer} --config #{installdir}\\zabbix_agentd.conf --install"
-		#not_if { File.exists?("#{node['perl']['install_dir']}\\perl\\bin\\perl.exe") }
-	end
-end
+#require 'win32ole'
+#zabbixservice = 'Zabbix Agent'
+#wmi = WIN32OLE.connect("winmgmts://")
+#services = wmi.ExecQuery("Select * from Win32_Service where Name = '#{zabbixservice}'")
+#if services.count >= 1
+#	#service exists
+#else
+#	#INSTALL AWAY!
+#	execute "Install Zabbix" do
+#		command "#{installdir}\\#{installer} --config #{installdir}\\zabbix_agentd.conf --install"
+#		#not_if { File.exists?("#{node['perl']['install_dir']}\\perl\\bin\\perl.exe") }
+#	end
+#end
 
-service "Zabbix Agent" do
-  supports :status => true, :start => true, :stop => true, :restart => true
-  action [ :enable , :start]
-end
+#service "Zabbix Agent" do
+#  supports :status => true, :start => true, :stop => true, :restart => true
+#  action [ :enable , :start]
+#end
