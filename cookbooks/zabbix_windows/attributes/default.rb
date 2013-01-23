715aca5e804fbe0ed4ec89309df951160099224f
Andrey Khon <andrey.khon@clearscale.net>
2013-01-23 18:36:32 +0700

    Added required inputs

--- cookbooks/zabbix_windows/attributes/default.rb
+++ cookbooks/zabbix_windows/attributes/default.rb
@@ -5,12 +5,10 @@
 default[:zabbix_windows][:log_dir] = "C:\\zabbix\\agent\\log"
 default[:zabbix_windows][:run_dir] = "C:\\zabbix\\agent"
 
-default[:zabbix_windows][:install] = true
 default[:zabbix_windows][:version] = "2.0.3"
 default[:zabbix_windows][:branch] = "ZABBIX%20Latest%20Stable"
-default[:zabbix_windows][:server] = ["zabbixserver.domain.local"]
+default[:zabbix_windows][:server] = "zabbixserver.domain.local"
 default[:zabbix_windows][:hostname] = node.fqdn
-default[:zabbix_windows][:configure_options] = [ "--with-libcurl" ]
 default[:zabbix_windows][:install_method] = "prebuild"
 
 default[:zabbix_windows][:include_dir] = "C:\\zabbix\\agent\\agent_include"
--- cookbooks/zabbix_windows/metadata.rb
+++ cookbooks/zabbix_windows/metadata.rb
@@ -6,3 +6,52 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
 
 recipe "zabbix_windows::windows_agent", "Installs and configures Zabbix agent"
+
+attribute "zabbix_windows/server_path",
+  :display_name => "Zabbix Server",
+  :description => "Path to zabbix server e.g. https://mon1.cloudnoc.com",
+  :required => "optional",
+  :default => "http://mon1.cloudnoc.com",
+  :recipes => [ "zabbix_windows::windows_agent" ]
+
+attribute "zabbix_windows/proxy_host_name",
+  :display_name => "proxy_host_name",
+  :description => "Hostname of Proxy Server",
+  :required => "optional",
+  :default => "",
+  :recipes => [ "zabbix_windows::windows_agent" ]
+
+attribute "zabbix_windows/proxy_ip",
+  :display_name => "proxy_ip",
+  :description => "PRIVATE_IP of zabbix proxy server",
+  :required => "optional",
+  :default => "",
+  :recipes => [ "zabbix_windows::windows_agent" ]
+
+attribute "zabbix_windows/username",
+  :display_name => "username",
+  :description => "User name used for authentification",
+  :required => "optional",
+  :default => "",
+  :recipes => [ "zabbix_windows::windows_agent" ]
+
+attribute "zabbix_windows/password",
+  :display_name => "password",
+  :description => "Password name used for authentification",
+  :required => "optional",
+  :default => "",
+  :recipes => [ "zabbix_windows::windows_agent" ]
+
+attribute "zabbix_windows/client_contact",
+  :display_name => "client_contact",
+  :description => "Contact email for tickets",
+  :required => "optional",
+  :default => "",
+  :recipes => [ "zabbix_windows::windows_agent" ]
+
+attribute "zabbix_windows/clientname",
+  :display_name => "clientname",
+  :description => "Name of the client",
+  :required => "optional",
+  :default => "",
+  :recipes => [ "zabbix_windows::windows_agent" ]
